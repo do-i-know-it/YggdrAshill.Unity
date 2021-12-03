1c868fb1aae18314e496776c3a895e579d8ab9f1
know <do.i.know.it@yggdrashill.com>
2021-12-03 12:56:54 +0900

    Import PUN2

---
+++ Assets/YggdrAshill.Samples/Scripts/PhotonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace YggdrAshill.Samples
+{
+    internal sealed class PhotonController : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private PhotonView viewPrefab;
+
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+        }
+
+        public override void OnJoinedRoom()
+        {
+            PhotonNetwork.Instantiate(viewPrefab.name, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/YggdrAshill.Samples/Scripts/SynchronizedAvatarLifetimeScope.cs
@@ -0,0 +1,90 @@
+﻿using YggdrAshill.Nuadha.Unity;
+using YggdrAshill.Unity;
+using YggdrAshill.VContainer;
+using System;
+using UnityEngine;
+using Photon.Pun;
+using VContainer;
+using VContainer.Unity;
+
+namespace YggdrAshill.Samples
+{
+    internal sealed class SynchronizedAvatarLifetimeScope : LifetimeScope
+    {
+        [SerializeField] private PhotonView view;
+
+        [SerializeField] private Transform originTransform;
+        private Transform OriginTransform
+        {
+            get
+            {
+                if (originTransform == null)
+                {
+                    originTransform = transform;
+                }
+
+                return originTransform;
+            }
+        }
+
+        [SerializeField] private Transform headTransform;
+        private Transform HeadTransform
+        {
+            get
+            {
+                if (headTransform == null)
+                {
+                    throw new InvalidOperationException($"{nameof(headTransform)} is null.");
+                }
+
+                return headTransform;
+            }
+        }
+
+        [SerializeField] private Transform leftHandTransform;
+        private Transform LeftHandTransform
+        {
+            get
+            {
+                if (leftHandTransform == null)
+                {
+                    throw new InvalidOperationException($"{nameof(leftHandTransform)} is null.");
+                }
+
+                return leftHandTransform;
+            }
+        }
+
+        [SerializeField] private Transform rightTransform;
+        private Transform RightHandTransform
+        {
+            get
+            {
+                if (rightTransform == null)
+                {
+                    throw new InvalidOperationException($"{nameof(rightTransform)} is null.");
+                }
+
+                return rightTransform;
+            }
+        }
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            if (!view.IsMine)
+            {
+                Destroy(this);
+                return;
+            }
+
+            builder
+                .RegisterInstance(DeviceManagement.HumanPoseTracker.Hardware)
+                .AsSelf();
+            builder
+                .RegisterInstance(ToTrack.HumanPose(OriginTransform, HeadTransform, LeftHandTransform, RightHandTransform))
+                .AsSelf();
+
+            builder.RegisterEntryPoint<TrackHumanPoseEntryPoint>();
+        }
+    }
+}
