1f4cbb55827e9d89d56c0cc948b5d58f54c71817
know <do.i.know.it@yggdrashill.com>
2021-12-07 16:27:58 +0900

    Add scripts to grab objects

---
+++ Assets/YggdrAshill.Samples/Scripts/Grabbable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace YggdrAshill.Samples
+{
+    internal sealed class Grabbable : MonoBehaviour
+    {
+        [SerializeField] private Transform targetTransform;
+
+        private Transform previousParent;
+
+        public void Grab(Transform transform)
+        {
+            previousParent = targetTransform.parent;
+
+            targetTransform.parent = transform;
+        }
+
+        public void Release(Transform transform)
+        {
+            targetTransform.parent = previousParent;
+        }
+    }
+}
