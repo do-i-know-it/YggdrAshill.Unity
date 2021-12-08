4813ed49b83de3e749941bf14a315ccf005ce427
know <do.i.know.it@yggdrashill.com>
2021-12-08 19:11:47 +0900

    Modify implementation of grabber

--- Assets/YggdrAshill.Samples/Scripts/SynchronizedTrackedHumanPose.cs
+++ Assets/YggdrAshill.Samples/Scripts/SynchronizedTrackedHumanPose.cs
@@ -1,7 +1,6 @@
 ﻿using YggdrAshill.Unity;
 using YggdrAshill.Nuadha.Unity;
 using YggdrAshill.VContainer;
-using System;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -13,84 +12,16 @@ namespace YggdrAshill.Samples
     internal sealed class SynchronizedTrackedHumanPose : LifetimeScope
     {
         [SerializeField] private PhotonView originPhotonView;
-        private PhotonView OriginPhotonView
-        {
-            get
-            {
-                if (originPhotonView != null)
-                {
-                    return originPhotonView;
-                }
-
-                if (TryGetComponent(out originPhotonView))
-                {
-                    return originPhotonView;
-                }
-
-                throw new InvalidOperationException($"{nameof(OriginPhotonView)} is null.");
-            }
-        }
 
         [SerializeField] private PhotonView headPhotonView;
-        private PhotonView HeadPhotonView
-        {
-            get
-            {
-                if (headPhotonView == null)
-                {
-                    throw new InvalidOperationException($"{nameof(HeadPhotonView)} is null.");
-                }
-
-                if (headPhotonView == OriginPhotonView)
-                {
-                    throw new InvalidOperationException($"{nameof(HeadPhotonView)} is same as {nameof(OriginPhotonView)}.");
-                }
-
-                return headPhotonView;
-            }
-        }
-
+        
         [SerializeField] private PhotonView leftHandPhotonView;
-        private PhotonView LeftHandPhotonView
-        {
-            get
-            {
-                if (leftHandPhotonView == null)
-                {
-                    throw new InvalidOperationException($"{nameof(LeftHandPhotonView)} is null.");
-                }
 
-                if (leftHandPhotonView == OriginPhotonView)
-                {
-                    throw new InvalidOperationException($"{nameof(LeftHandPhotonView)} is same as {nameof(OriginPhotonView)}.");
-                }
-
-                return leftHandPhotonView;
-            }
-        }
-
-        [SerializeField] private PhotonView rightPhotonView;
-        private PhotonView RightHandPhotonView
-        {
-            get
-            {
-                if (rightPhotonView == null)
-                {
-                    throw new InvalidOperationException($"{nameof(RightHandPhotonView)} is null.");
-                }
-
-                if (rightPhotonView == OriginPhotonView)
-                {
-                    throw new InvalidOperationException($"{nameof(RightHandPhotonView)} is same as {nameof(OriginPhotonView)}.");
-                }
-
-                return rightPhotonView;
-            }
-        }
+        [SerializeField] private PhotonView rightHandPhotonView;
 
         protected override void Configure(IContainerBuilder builder)
         {
-            if (!OriginPhotonView.IsMine)
+            if (!originPhotonView.IsMine)
             {
                 Destroy(this);
 
@@ -101,7 +32,7 @@ namespace YggdrAshill.Samples
                 .RegisterInstance(DeviceManagement.HumanPoseTracker.Hardware)
                 .AsSelf();
             builder
-                .RegisterInstance(ToTrack.HumanPose(OriginPhotonView.transform, HeadPhotonView.transform, LeftHandPhotonView.transform, RightHandPhotonView.transform))
+                .RegisterInstance(ToTrack.HumanPose(originPhotonView.transform, headPhotonView.transform, leftHandPhotonView.transform, rightHandPhotonView.transform))
                 .AsSelf();
 
             builder.RegisterEntryPoint<TrackHumanPoseEntryPoint>();
