5e3a7e83dc7e09b71298e80ec9fe7446fb1109bc
know <do.i.know.it@yggdrashill.com>
2022-02-21 10:23:56 +0900

    Add texts for license description of thrid party libraries

---
+++ Assets/YggdrAshill.Samples/Scripts/ToggleLicenseDescription.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace YggdrAshill.Samples
+{
+    internal sealed class ToggleLicenseDescription : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private GameObject target;
+
+        private void Toggle()
+        {
+            target.SetActive(!target.activeInHierarchy);
+        }
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(Toggle);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(Toggle);
+        }
+    }
+}
